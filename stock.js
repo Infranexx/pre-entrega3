a2fc08bd65046d093fb9da8ce552ed1786bf596a
Lucas Biscotti <lucasbiscotti1602@gmail.com>
2024-07-09 22:31:20 -0400

    1

---
+++ stock.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Detergente",
+        precio: 5000,
+        cantidad: 0
+    },
+    {
+        id: 2,
+        nombre: "Suavizante",
+        precio: 6000,
+        cantidad: 0,
+    },
+    { 
+        id: 3,
+        nombre: "Ambiental aromas",
+        precio: 8000,
+        cantidad: 0,
+    },
+    {
+        id: 4,
+        nombre: "Limpia pisos Aromas",
+        precio: 4000,
+        cantidad: 0,
+    },
+    {
+        id: 5,
+        nombre: "Desmanchador líquido",
+        precio: 6000,
+        cantidad:0,
+    }
+]
